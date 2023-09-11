da3a7dbf386ce7210bb27f180fd7c06e314648e7
haeussma <83341109+haeussma@users.noreply.github.com>
2023-09-11 10:06:02 +0200

    added well getters

--- PlateHandler/core/well.py
+++ PlateHandler/core/well.py
@@ -109,9 +109,24 @@ class Well(sdRDM.DataModel):
 
         for condition in self.init_conditions:
             if condition.species == species.id:
+                print("yes therer is species")
                 return True
 
         return False
+
+    def _all_blanked_except_species(self, species: Species) -> bool:
+
+        other_species_blanked = []
+        for condition in self.init_conditions:
+            if condition.species == species.id:
+                species_was_blanked = condition.was_blanked
+            else:
+                other_species_blanked.append(condition.was_blanked)
+
+        if not species_was_blanked and all(other_species_blanked):
+            return True
+        else:
+            return False
 
     def _get_species_condition(self, species: Species) -> InitCondition:
 
--- PlateHandler/ioutils/standardmapper.py
+++ PlateHandler/ioutils/standardmapper.py
@@ -1,26 +1,68 @@
 from typing import List
 from collections import defaultdict
+
+
 from CaliPytion.core.standard import Standard
 from PlateHandler.core.species import Species
-
+from PlateHandler.core.plate import Plate
 from PlateHandler.core.well import Well
 
 
-def wells_to_standard(
-        wells: List[Well],
-        species: Species
+def create_standard(
+        plate: Plate,
+        species: Species,
+        wavelength: int = None,
 ) -> Standard:
 
-    # check if all wells contain the species
-    if not all(well._contains_species(species) for well in wells):
-        raise AttributeError(
-            f"All wells must contain species {species.name}."
-        )
+    # handel wavelength
+    if not wavelength:
+        if len(plate.measured_wavelengths) == 1:
+            wavelength = plate.measured_wavelengths[0]
+        else:
+            raise AttributeError(
+                f"Argument 'wavelength' must be provided. Measured wavelengths are: {plate.measured_wavelengths}"
+            )
 
-    # identify replicates
-    conc_well_id_dict = defaultdict(list)
+    wells = plate.get_wells(wavelength=wavelength)
+    print(f"all wells: {len(wells)}")
+
+    # get wells, of species if no enzyme is present and 'was_blanked' is True
+    # for all species except the one of interest.
+
+    standard_wells = []
     for well in wells:
-        condition = well._get_species_condition(species)
-        conc_well_id_dict[condition.init_conc].append(well.id)
+        if not well._contains_species(species):
+            print(f"no species {well.id, species.id}")
+            continue
 
-    return conc_well_id_dict
+        if plate._get_catalyst_id():
+            print("yes has catalyst")
+            catalyst_id = plate._get_catalyst_id()
+            if well._contains_species(catalyst_id):
+                print(f"catalyst {well.id}")
+                continue
+
+        if well._contains_catalyst():
+            print(f"catalyst {well.id}")
+            continue
+        if well._all_blanked_except_species(species):
+            standard_wells.append(well)
+
+    return standard_wells
+
+    # # check if all wells contain the species
+    # wells = [well for well in wells if well.contains_species(species)]
+
+    # # group wells by same initial concentration
+    # conc_well_id_dict = defaultdict(list)
+    # for well in wells:
+    #     condition = well._get_species_condition(species)
+    #     conc_well_id_dict[condition.init_conc].append(well.id)
+
+    # concentrations = list(conc_well_id_dict.keys())
+    # absorption_replicate_ids = list(map(list, zip(*conc_well_id_dict.values()))
+    #                    )  # transpose list of well ids
+
+    # # create standard
+    # standard = Standard(
+    #     wavelength=
