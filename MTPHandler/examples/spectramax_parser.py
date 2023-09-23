773c5c8cf5997c9eb6e6162644a0d74b591cd205
haeussma <83341109+haeussma@users.noreply.github.com>
2023-09-23 18:42:55 +0200

    fixed spectramax ph issue

--- MTPHandler/examples/spectramax_parser.py
+++ MTPHandler/examples/spectramax_parser.py
@@ -10,37 +10,37 @@ import pandas as pd
 
 
 def parse_spectramax(
-        cls: "Plate",
-        path: str,
-        time: float | np.ndarray,
-        time_unit: str,
-        ph: float = None,
-        temperature: float = None,
-        temperature_unit: str = None,
+    cls: "Plate",
+    path: str,
+    time: float | np.ndarray,
+    time_unit: str,
+    ph: float = None,
+    temperature: float = None,
+    temperature_unit: str = None,
 ) -> "Plate":
-
     # Get date of measurement
-    created = datetime.fromtimestamp(os.path.getctime(
-        path)).strftime('%Y-%m-%d %H:%M:%S')
+    created = datetime.fromtimestamp(os.path.getctime(path)).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
 
     # Extract temperature from path
     if not temperature:
-        TEMP_PATTERN = r'\d{1,3}deg'
+        TEMP_PATTERN = r"\d{1,3}deg"
         temperature = re.findall(TEMP_PATTERN, path)[0]
-        temperature = re.split('(\d+)', temperature)[1]
+        temperature = re.split("(\d+)", temperature)[1]
         if not temperature:
-            raise ValueError('Could not find pH in path. Please specify pH.')
+            raise ValueError("Could not find pH in path. Please specify pH.")
 
     if not temperature_unit:
-        temperature_unit = 'C'
+        temperature_unit = "C"
 
     # Extract pH from path
     if not ph:
-        PH_PATTERN = r'pH\d{1,2}'
+        PH_PATTERN = r"pH\d+\.\d+"
         ph = re.findall(PH_PATTERN, path)[0]
-        ph = re.split('(\d+)', ph)[1]
+        ph = float(re.search(r"\d+\.\d+", ph).group())
         if not ph:
-            raise ValueError('Could not find pH in path. Please specify pH.')
+            raise ValueError("Could not find pH in path. Please specify pH.")
 
     # Read file
     data_dict = extract_data(pd.read_csv(path).reset_index())
@@ -87,14 +87,12 @@ def parse_spectramax(
 
 
 def extract_data(df: pd.DataFrame) -> Dict[int, List[List[float]]]:
-
     # Get slices of the data corresponding each iteration of measurement
     wavelength_df_dict = _get_plate_dfs(df)
 
     # Extract data from each iteration
     wavelength_data_dict = defaultdict(list)
     for wavelength, dfs in wavelength_df_dict.items():
-
         for df in dfs:
             data_of_iteration = df.apply(_extract_row_data, axis=1)
             data_of_iteration = np.array(data_of_iteration.to_list())
@@ -103,8 +101,7 @@ def extract_data(df: pd.DataFrame) -> Dict[int, List[List[float]]]:
 
     # Convert data to numpy arrays
     for wavelength, data in wavelength_data_dict.items():
-        wavelength_data_dict[wavelength] = np.array(
-            data).swapaxes(0, 2).swapaxes(0, 1)
+        wavelength_data_dict[wavelength] = np.array(data).swapaxes(0, 2).swapaxes(0, 1)
 
     return wavelength_data_dict
 
@@ -113,19 +110,15 @@ def _get_plate_dfs(df: pd.DataFrame) -> Dict[int, List[pd.DataFrame]]:
     wavelength_slices_dict = defaultdict(list)
 
     # Get data by wavelength
-    wavelengths = df.apply(
-        _get_wavelengths, axis=1).dropna().astype(int).tolist()
+    wavelengths = df.apply(_get_wavelengths, axis=1).dropna().astype(int).tolist()
 
     wavelength_dfs = _segment_dataframe(df, "Wavelength")
 
     for wavelength, wavelength_df in zip(wavelengths, wavelength_dfs):
-
-        iteration_dfs = _segment_dataframe(
-            wavelength_df, "Iteration")
+        iteration_dfs = _segment_dataframe(wavelength_df, "Iteration")
 
         for iteration_df in iteration_dfs:
-            wavelength_slices_dict[wavelength].append(
-                iteration_df)
+            wavelength_slices_dict[wavelength].append(iteration_df)
 
     return wavelength_slices_dict
 
@@ -136,14 +129,12 @@ def _extract_row_data(row: pd.Series):
 
 
 def _segment_dataframe(df: pd.DataFrame, key: str):
-
     def filter_function(row: pd.Series):
         row_id, value = row.values
         if key in value:
             return row_id
 
-    slice_ids = df.apply(filter_function, axis=1).dropna().astype(
-        int).values.tolist()
+    slice_ids = df.apply(filter_function, axis=1).dropna().astype(int).values.tolist()
 
     list_of_slices = _list_to_slices(slice_ids)
 
@@ -153,10 +144,9 @@ def _segment_dataframe(df: pd.DataFrame, key: str):
 def _list_to_slices(_list: List[int]) -> List[slice]:
     slices = []
     for idx, element in enumerate(_list):
-
         try:
             start_data_slice = element + 1
-            end_data_slice = _list[idx+1] - 1
+            end_data_slice = _list[idx + 1] - 1
             slices.append(slice(start_data_slice, end_data_slice))
 
         except IndexError:
@@ -168,13 +158,10 @@ def _list_to_slices(_list: List[int]) -> List[slice]:
 
 
 def _get_wavelengths(row: pd.Series):
-
     value = row.values[1]
 
     if "Wavelength" in value:
-
-        return re.findall(
-            r"Wavelength:(.*)", value)[0]
+        return re.findall(r"Wavelength:(.*)", value)[0]
 
 
 def _coordinates_to_id(x: int, y: int) -> str:
