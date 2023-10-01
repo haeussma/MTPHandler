cda1a6f6eb56284d686892f4d07153055437c239
haeussma <83341109+haeussma@users.noreply.github.com>
2023-10-01 13:29:49 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mtphandler',
-    version='0.1.0',
-    packages=find_packages(include=['PlateHandler', 'PlateHandler.*']),
-    install_requires=[
-        'numpy',
-        'sympy',
-        'pandas',
-        'matplotlib',
-        'plotly'
-    ],
+    name="MTPHandler",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["numpy", "sympy", "pandas", "matplotlib", "plotly"],
     extras_require={
-        'JupyterNotebook': ['jupyter', 'ipykernel'],
-    }
-
-
+        "JupyterNotebook": ["jupyter", "ipykernel"],
+    },
 )
